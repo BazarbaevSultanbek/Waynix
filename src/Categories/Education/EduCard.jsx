792e5b606de68f8d32ea319d4f5d379ce4da0ed3
Stephen Aliev <bazarbaeoff@gmail.com>
2026-02-18 00:28:10 -0500

    Update import path for education styles

--- src/Categories/Education/EduCard.jsx
+++ src/Categories/Education/EduCard.jsx
@@ -7,7 +7,7 @@ import {
   IconClock,
 } from "@tabler/icons-react";
 import { Card, Image, Group, Button, Text, Anchor, Badge } from "@mantine/core";
-import "../../utils/styles/education.scss";
+import "@/utils/styles/education.scss";
 
 function getIsOpen(hours) {
   const [start, end] = hours.split("-");
